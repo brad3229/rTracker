a067af36c4e5d4018c13fa44369fe757d18d9541
brad3229 <87099526+brad3229@users.noreply.github.com>
2023-04-12 02:55:52 -0400

    Add files via upload

---
+++ `data_insert`.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `data_insert` (  
+  `id` int(11) NOT NULL,  
+  `CompanyName` varchar(100) NOT NULL,  
+  `CompanyEmail` varchar(150) NOT NULL,
+  `Item` varchar(30) NOT NULL,
+  `ExpDate` varchar(30) NOT NULL,
+  `Message` varchar(100) NOT NULL  
+ ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
